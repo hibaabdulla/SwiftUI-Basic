174d69c16226799d00bf52c6856654548c115593
Hiba Abdulla <hibaabdullahkk@gmail.com>
2024-05-05 20:38:27 -0700

    Initial Commit

---
+++ SwiftUI Basic/SwiftUI_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicApp.swift
+//  SwiftUI Basic
+//
+//  Created by Hiba Abdulla on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
